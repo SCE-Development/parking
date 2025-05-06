d5886e89aef0c3589351d832f767f6924dd1fe63
Yuwen Zhang <yuwen.x.zhang@gmailcom>
2025-05-05 18:36:09 -0700

    initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true
+    },
+    proxy: {
+      '/parking': 'http://localhost:8000',
+      '/parking-history': 'http://localhost:8000'
+    }
+  }
+})
