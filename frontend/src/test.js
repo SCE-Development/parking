56c21f6281afc2db763a7cf4c34fe956387405e2
Yuwen Zhang <yuwen.x.zhang@gmailcom>
2025-05-06 16:49:52 -0700

    deleted test.js

--- frontend/src/test.js
+++
@@ -1,5 +0,0 @@
-const currentDate = new Date();
-const month = (currentDate.getMonth() + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-const day = currentDate.getDate().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-
-console.log(`Current Date: ${month}/${day}`);
